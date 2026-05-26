c2c677e673d3d65df01e7647c39a3dbb07cf22c6
abdulazis06 <muhammadfatihahi4@gmail.com>
2026-05-26 15:24:36 +0700

    tambah file php oop

---
+++ acces_modifier.php
@@ -0,0 +1,14 @@
+<?php
+class Fruit {
+  public $name;
+
+  public function get_details() {
+    echo "Name: " . $this->name . ".";
+  }
+}
+
+$apple = new Fruit();
+$apple->name = "Apple"; // Can be accessed directly
+$apple->get_details();
+?>
+
---
+++ static properties.php
@@ -0,0 +1,16 @@
+<?php
+
+class ClassName {
+  public static $statProp = "W3Schools";
+}
+
+ClassName::$statProp;
+
+class pi {
+  // static property
+  public static $value = 3.14159;
+}
+
+// Get static property directly
+echo pi::$value;
+?>
